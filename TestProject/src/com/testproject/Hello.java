d86b6def4a87aae2c108d90a94397b10fd118bb8
jason.sixsteps <jason.sixsteps@qq.com>
2014-03-06 14:58:54 +0800

    Change-Id: I88aea999bec3938c83e17dc6674b5272dea7ab68 Signed-off-by: jason.sixsteps <jason.sixsteps@qq.com>

---
+++ TestProject/src/com/testproject/Hello.java
@@ -0,0 +1,22 @@
+/**
+ * @author Jason.w  2014/3/6
+ * @description
+ *   
+ */
+package com.testproject;
+
+/**
+ * This is a sample class just for testing the env.
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 * @description
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello! New ENV in windows7!");
+
+	}
+
+}
